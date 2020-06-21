8fb0f5367e249574b50c41375f72a7240f58802d
FernandoLisb10 <fernandolisb10@gmail.com>
2020-06-21 19:34:06 -0300

    criamos a classe Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -9,26 +9,24 @@ public function __construct(){
    $this->conexao = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "123");
 }
 
-private function setParams($statment, $parameters = array()){
+private function setParams($statement, $parameters = array()){
       
     foreach ($parameters as $key => $value) {
 	
-      $this->setParam($key, $value);
+      $this->setParam($statement, $key, $value);
 
     }
 
 }
 
-private function setParam($statment, $key, $value){
+private function setParam($statement, $key, $value){
 
-	$statment->binParam($key, $value);
+	$statement->binParam($key, $value);
 }
 
 public function query($rawQuery, $params = array()){
 
      $stmt = $this->conexao->prepare($rawQuery);
-
-     $this->setParams($stmt, $params);
 
      $this->setParams($stmt, $params);
 
@@ -43,10 +41,8 @@ public function select($rawQuery, $params = array()):array
     $stmt = $this->query($rawQuery, $params);
 
     return $stmt->fetchAll(PDO:: FETCH_ASSOC);
-
-
 }
 
 }
 
- ?>
+?>
--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 require_once("config.php");
 
+// Código comentado falta econtrar o erro!
+//$user = new Usuario();
+//$user->loadbyId(3);
+//echo $user;
+
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-
 echo json_encode($usuarios);
 
- ?>
+?>
+
+
